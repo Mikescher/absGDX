c8d0552fb2ec3949bfb9645d4207b327d4fa651a
Mike Schwörer <mailport@mikescher.de>
2014-10-19 19:22:44 +0200

    Gradle Trickeries and more

---
+++ absGDX-framework/src/de/samdev/absgdx/AndroidResolutions.java
@@ -0,0 +1,17 @@
+package de.samdev.absgdx;
+
+import com.badlogic.gdx.math.GridPoint2;
+
+public final class AndroidResolutions {
+
+	public final static GridPoint2 RES_4_3 = new GridPoint2(320, 240); // 4:3
+	public final static GridPoint2 RES_9_5 = new GridPoint2(432, 240); // 9:5
+	public final static GridPoint2 RES_3_2 = new GridPoint2(480, 320); // 3:2
+	public final static GridPoint2 RES_5_3 = new GridPoint2(800, 480); // 5:3
+	public final static GridPoint2 RES__16_9 = new GridPoint2(1280, 720); // 16:9
+	public final static GridPoint2 RES_25_16 = new GridPoint2(1200, 768); // 25:16
+	public final static GridPoint2 RES_8_5 = new GridPoint2(1280, 800); // 8:5
+	public final static GridPoint2 RES_14_9 = new GridPoint2(1400, 900); // 14:9
+
+	public final static GridPoint2[] RES_ALL = new GridPoint2[] { RES_4_3, RES_9_5, RES_3_2, RES_5_3, RES__16_9, RES_25_16, RES_8_5, RES_14_9 };
+}
--- desktop/src/de/samdev/absgdx/desktop/DesktopLauncher.java
+++ desktop/src/de/samdev/absgdx/desktop/DesktopLauncher.java
@@ -2,11 +2,20 @@ package de.samdev.absgdx.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.math.GridPoint2;
+
 import de.samdev.absgdx.AGdxDemoGame;
+import de.samdev.absgdx.AndroidResolutions;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		GridPoint2 resolution = AndroidResolutions.RES__16_9;
+		
+		config.width  = resolution.x;
+		config.height = resolution.y;
+		
 		new LwjglApplication(new AGdxDemoGame(), config);
 	}
 }

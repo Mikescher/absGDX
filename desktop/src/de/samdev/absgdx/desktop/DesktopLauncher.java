80dbeef9736d87cc095a59898c39f9c77b3018c7
Mike Schwörer <mailport@mikescher.de>
2014-10-19 13:38:01 +0200

    Added project sceleton

---
+++ desktop/src/de/samdev/absgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.samdev.absgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.samdev.absgdx.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}

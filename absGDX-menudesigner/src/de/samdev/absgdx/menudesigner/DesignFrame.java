9d9cca60be9b5a6e446119439c1921ced3da06e7
Mike Schwörer <mailport@mikescher.de>
2015-03-02 23:51:11 +0100

    Started implementing MenuDesigner

--- absGDX-menudesigner/src/de/samdev/absgdx/menudesigner/DesignFrame.java
+++ absGDX-menudesigner/src/de/samdev/absgdx/menudesigner/DesignFrame.java
@@ -1,23 +1,81 @@
 package de.samdev.absgdx.menudesigner;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import javax.swing.JLabel;
 
 public class DesignFrame extends JFrame {
 	private static final long serialVersionUID = 5936312660450931611L;
 	
 	private JPanel contentPane;
+	private JSplitPane splitPane;
+	private JLabel lblDraw;
+	private JPanel pnlBottom;
+	private JTabbedPane pnlSettings;
+	private RSyntaxTextArea edCode;
+	private JList list;
+	private JPanel tabSettings;
+	private JPanel tabComponents;
+	private Component horizontalStrut;
 
 	public DesignFrame() {
+		initGUI();
+	}
+	
+	private void initGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 949, 724);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.5);
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		contentPane.add(splitPane, BorderLayout.CENTER);
+		
+		lblDraw = new JLabel();
+		splitPane.setLeftComponent(lblDraw);
+		lblDraw.setLayout(null);
+		
+		pnlBottom = new JPanel();
+		splitPane.setRightComponent(pnlBottom);
+		pnlBottom.setLayout(new BorderLayout(0, 0));
+		
+		edCode = new RSyntaxTextArea();
+		edCode.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_XML);
+		edCode.setCodeFoldingEnabled(true);
+		pnlBottom.add(edCode, BorderLayout.CENTER);
+		
+		pnlSettings = new JTabbedPane(JTabbedPane.TOP);
+		pnlSettings.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		pnlBottom.add(pnlSettings, BorderLayout.EAST);
+		
+		tabComponents = new JPanel();
+		pnlSettings.addTab("Components", null, tabComponents, null);
+		tabComponents.setLayout(new BorderLayout(0, 0));
+		
+		list = new JList();
+		tabComponents.add(list);
+		
+		tabSettings = new JPanel();
+		pnlSettings.addTab("Settings", null, tabSettings, null);
+		
+		horizontalStrut = Box.createHorizontalStrut(200);
+		tabSettings.add(horizontalStrut);
 	}
-
 }
---
+++ absGDX-menudesigner/src/de/samdev/absgdx/menudesigner/GUIPreviewPanel.java
@@ -0,0 +1,28 @@
+package de.samdev.absgdx.menudesigner;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class GUIPreviewPanel extends JPanel {
+
+	public GUIPreviewPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUIPreviewPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUIPreviewPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUIPreviewPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
